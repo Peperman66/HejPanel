eac5a1c8b2ba009ad5c471d625deb162e74e3f52
Jakub Ondroušek <peperman66@seznam.cz>
2021-06-05 17:45:52 +0200

    add basic static oak server

---
+++ index.ts
@@ -0,0 +1,14 @@
+import { Application } from 'https://deno.land/x/oak/mod.ts';
+import { config } from 'https://deno.land/x/dotenv/mod.ts';
+
+const app = new Application();
+const port: number = parseInt(config({ safe: true }).PORT);
+
+app.use((ctx) => {
+    ctx.send({
+        root: `${Deno.cwd()}/static`,
+        index: 'index.html'
+    });
+});
+
+await app.listen({ port: port });
