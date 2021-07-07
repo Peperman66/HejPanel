91f41ae0948367b7b0b01fbff5256128feca8e4f
Peperman66 <peperman66@seznam.cz>
2021-07-07 16:01:16 +0200

    add version in import urls

--- index.ts
+++ index.ts
@@ -1,5 +1,5 @@
-import { Application } from 'https://deno.land/x/oak/mod.ts';
-import { config } from 'https://deno.land/x/dotenv/mod.ts';
+import { Application } from 'https://deno.land/x/oak@v7.7.0/mod.ts';
+import { config } from 'https://deno.land/x/dotenv@v2.0.0/mod.ts';
 
 const app = new Application();
 const port: number = parseInt(config({ safe: true }).PORT);
