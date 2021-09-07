cc949b174643716922e8be5a4ebe9bf430624297
Peperman66 <peperman66@seznam.cz>
2021-09-07 20:53:22 +0200

    add lib for calling on specified time

---
+++ libs/time_call.ts
@@ -0,0 +1,5 @@
+export function CallOnSetTime(callback: ()=>void, millisPastHour: number): number {
+    const now = new Date();
+    const diff = (millisPastHour + 60*60*1000 /*One hour*/ - (now.getMinutes() * 60*1000 + now.getSeconds() * 1000 + now.getMilliseconds())) % 60*60*1000 //One hour
+    return setTimeout(callback, diff);
+}
