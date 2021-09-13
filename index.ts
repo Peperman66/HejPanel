009e3f77dd537da962c2e7d20f38eb4318fdfb64
Peperman66 <peperman66@seznam.cz>
2021-09-13 13:04:12 +0200

    add basic suport for ssl and https

--- index.ts
+++ index.ts
@@ -1,10 +1,10 @@
-import { Application } from 'https://deno.land/x/oak@v7.7.0/mod.ts';
+import { Application, ListenOptions, ListenOptionsTls } from 'https://deno.land/x/oak@v7.7.0/mod.ts';
 import { config } from 'https://deno.land/x/dotenv@v2.0.0/mod.ts';
 import apiRouter from './routers/api.ts';
 
 const app = new Application();
-const port: number = parseInt(config({ safe: true }).PORT);
-const login: string = config({safe: true}).LOGIN;
+const env = config({safe: true});
+const port: number = parseInt(env.PORT);
 
 app.use(apiRouter.routes());
 app.use(apiRouter.allowedMethods());
@@ -12,7 +12,7 @@ app.use(apiRouter.allowedMethods());
 app.use(async (ctx, next) => {
     if (ctx.request.method === 'GET') {
         if (ctx.request.url.pathname === '/edit.html') {
-            if (ctx.request.headers.get('Authorization')?.split(' ')[1] !== login) {
+            if (ctx.request.headers.get('Authorization')?.split(' ')[1] !== env.LOGIN) {
                 ctx.response.status = 401;
                 ctx.response.headers.set('WWW-Authenticate', 'Basic realm="HejPanel Login"');
                 return;
@@ -42,4 +42,16 @@ app.use(async (ctx) => {
     });
 });
 
-await app.listen({ port: port });
+let settings: ListenOptions = {
+    port: port
+}
+
+if (env.HTTPS_CERTFILELOCATION !== null) {
+    // Convert to ListnOptionsTls, I didn't find any other way
+    settings = (settings as ListenOptions) as ListenOptionsTls;
+    settings.secure = true;
+    settings.certFile = env.HTTPS_CERTFILELOCATION;
+    settings.keyFile = env.HTTPS_KEYFILELOCATION;
+}
+
+await app.listen(settings);
