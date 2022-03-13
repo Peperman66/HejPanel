770376f3a45f6d46eaf69f4cd8a14c53a3610430
Peperman66 <peperman66@seznam.cz>
2022-03-13 15:26:06 +0100

    fix transactions begin

--- handlers/events.ts
+++ handlers/events.ts
@@ -10,23 +10,24 @@ export type Events = Array<Event>;
 export async function SaveEventData(data: Events): Promise<void> {
     const client = await pool.connect();
     const transaction = client.createTransaction("save_events");
+    await transaction.begin();
     client.queryArray('TRUNCATE TABLE events;');
 
     for (let i = 0; i < data.length; i++) {
         const currentEvent = data[i];
         client.queryArray(`INSERT INTO events (Id, Name, Description, IsImportant) VALUES (${i}, ${currentEvent.Name}, ${currentEvent.Description}, ${currentEvent.IsImportant});`);
     }
-    await client.end()
+    client.release()
     return transaction.commit();
 }
 
 export async function GetEventData(): Promise<Events> {
     const client = await pool.connect();
     const transaction = client.createTransaction("read_events", {read_only: true});
-
+    await transaction.begin();
     const result = await client.queryObject<Event>('SELECT Name, Description, IsImportant FROM events ORDER BY Id;');
     await transaction.commit();
-    await client.end();
+    client.release();
     return result.rows;
 }
 
--- handlers/media.ts
+++ handlers/media.ts
@@ -9,6 +9,7 @@ export type Images = Array<Image>;
 export async function SaveMediaData(data: Images): Promise<void> {
     const client = await pool.connect();
     const transaction = client.createTransaction("save_media");
+    await transaction.begin();
     client.queryArray('TRUNCATE TABLE media;');
 
     for (let i = 0; i < data.length; i++) {
@@ -16,15 +17,16 @@ export async function SaveMediaData(data: Images): Promise<void> {
         const imageDuration = data[i].Duration;
         client.queryArray(`INSERT INTO media (Id, Image, Duration) VALUES (${i}, ${imageData}, ${imageDuration});`);
     }
-    await client.end();
+    client.release();
     return transaction.commit();
 }
 
 export async function GetMediaData(): Promise<Images> {
     const client = await pool.connect();
     const transaction = client.createTransaction("read_media", { read_only: true });
+    await transaction.begin();
     const data = await client.queryObject<Image>('SELECT Image, Duration FROM media ORDER BY ID;');
     await transaction.commit();
-    await client.end();
+    client.release();
     return data.rows;
 }
