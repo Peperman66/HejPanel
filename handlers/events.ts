ca16f1597b77ba57ae608db1b65631837cfd20c1
Peperman66 <peperman66@seznam.cz>
2022-03-13 16:03:31 +0100

    fix transactions and statements not in transactions

--- handlers/events.ts
+++ handlers/events.ts
@@ -9,23 +9,23 @@ export type Events = Array<Event>;
 
 export async function SaveEventData(data: Events): Promise<void> {
     const client = await pool.connect();
-    const transaction = client.createTransaction("save_events");
+    const transaction = client.createTransaction("save_events", {isolation_level: "serializable"});
     await transaction.begin();
-    client.queryArray('TRUNCATE TABLE events;');
+    transaction.queryArray('TRUNCATE TABLE events;');
 
     for (let i = 0; i < data.length; i++) {
         const currentEvent = data[i];
         client.queryArray(`INSERT INTO events (Id, Name, Description, IsImportant) VALUES (${i}, ${currentEvent.Name}, ${currentEvent.Description}, ${currentEvent.IsImportant});`);
     }
-    client.release()
-    return transaction.commit();
+    await transaction.commit();
+    client.release();
 }
 
 export async function GetEventData(): Promise<Events> {
     const client = await pool.connect();
     const transaction = client.createTransaction("read_events", {read_only: true});
     await transaction.begin();
-    const result = await client.queryObject<Event>('SELECT Name, Description, IsImportant FROM events ORDER BY Id;');
+    const result = await transaction.queryObject<Event>('SELECT Name, Description, IsImportant FROM events ORDER BY Id;');
     await transaction.commit();
     client.release();
     return result.rows;
--- handlers/media.ts
+++ handlers/media.ts
@@ -8,24 +8,24 @@ export type Images = Array<Image>;
 
 export async function SaveMediaData(data: Images): Promise<void> {
     const client = await pool.connect();
-    const transaction = client.createTransaction("save_media");
+    const transaction = client.createTransaction("save_media", {isolation_level: "serializable"});
     await transaction.begin();
-    client.queryArray('TRUNCATE TABLE media;');
+    transaction.queryArray('TRUNCATE TABLE media;');
 
     for (let i = 0; i < data.length; i++) {
         const imageData = data[i].Image;
         const imageDuration = data[i].Duration;
         client.queryArray(`INSERT INTO media (Id, Image, Duration) VALUES (${i}, ${imageData}, ${imageDuration});`);
     }
+    await transaction.commit();
     client.release();
-    return transaction.commit();
 }
 
 export async function GetMediaData(): Promise<Images> {
     const client = await pool.connect();
     const transaction = client.createTransaction("read_media", { read_only: true });
     await transaction.begin();
-    const data = await client.queryObject<Image>('SELECT Image, Duration FROM media ORDER BY ID;');
+    const data = await transaction.queryObject<Image>('SELECT Image, Duration FROM media ORDER BY ID;');
     await transaction.commit();
     client.release();
     return data.rows;
