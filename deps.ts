4eec08157816ec9df457dc0e7f4b2632e89f2a58
Peperman66 <peperman66@seznam.cz>
2023-12-12 08:36:16 +0100

    upgrade deno version

--- deps.ts
+++ deps.ts
@@ -1,8 +1,8 @@
-export {Router, Application} from "https://deno.land/x/oak@v11.1.0/mod.ts";
-export {assert} from "https://deno.land/std@0.166.0/testing/asserts.ts";
-export { DOMParser } from "https://deno.land/x/deno_dom@v0.1.36-alpha/deno-dom-wasm.ts";
-export {Buffer} from 'https://deno.land/std@0.166.0/node/buffer.ts'
-export {config} from 'https://deno.land/std@0.166.0/dotenv/mod.ts'
-export {decode} from 'https://deno.land/std@0.166.0/encoding/base64.ts'
+export {Router, Application} from "https://deno.land/x/oak@v12.6.0/mod.ts";
+export {assert} from "https://deno.land/std@0.208.0/testing/asserts.ts";
+export { DOMParser } from "https://deno.land/x/deno_dom@v0.1.43/deno-dom-wasm.ts";
+export {Buffer} from 'https://deno.land/std@0.208.0/node/buffer.ts'
+export {config} from 'https://deno.land/std@0.208.0/dotenv/mod.ts'
+export {decode} from 'https://deno.land/std@0.208.0/encoding/base64.ts'
 
-export type {ListenOptions, ListenOptionsTls} from "https://deno.land/x/oak@v11.1.0/mod.ts";
+export type {ListenOptions, ListenOptionsTls} from "https://deno.land/x/oak@v12.6.0/mod.ts";
